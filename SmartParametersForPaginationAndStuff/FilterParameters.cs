cbca05527fca9494e0122e1b2eb25fbd76d99104
Josbourne2 <jos.menhart@live.nl>
2021-04-15 11:25:18 +0200

    Add project files.

---
+++ SmartParametersForPaginationAndStuff/FilterParameters.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartParametersForPaginationAndStuff.Pages
+{
+    public class FilterParameters
+    {
+        public string SearchTerm { get; set; }
+    }
+}
---
+++ SmartParametersForPaginationAndStuff/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartParametersForPaginationAndStuff
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public int Age { get; set; }
+        public string EyeColor { get; set; }
+        public string Name { get; set; }
+        public int MyProperty { get; set; }
+        public GenderEnum Gender { get; set; }
+    }
+
+    public enum GenderEnum
+    {
+        UNKNOWN = 0,
+        FEMALE = 1,
+        MALE = 2
+    }
+}
